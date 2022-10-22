4a608cb046111aa36f2e02a45d0f0c9348ace25a
Petrut <ivanoiu.petrut27@yahoo.com>
2022-10-22 13:03:02 +0300

    feat: add simple route for get /song

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const app = require("express")();
 const PORT = 8080;
 
+// * Import the routes
+
+const songRoute = require("./routes/songs");
+app.use("/song", songRoute);
+
+// * Routes
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home 🏠");
+});
+
 // * Start the server
 app.listen(PORT, () => {
   console.log(`Server it's alive on http://localhost:${PORT}`);
